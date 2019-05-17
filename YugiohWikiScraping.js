3bacf413c35c1f6c689430cb5f5fe92239321777
Rutile3 <Rutile3@outlook.jp>
2019-05-17 09:55:19 +0900

    遊戯王Wikiから転載するブックマークレットを作成しました。

--- YugiohWikiScraping.js
+++ YugiohWikiScraping.js
@@ -1,6 +1,11 @@
 
 javascript: (function () {
-  const page_title = document.querySelector("#header > h1 > a").innerHTML;
-  let result = page_title.replace(/《/g , "").replace(/》/g , "");
-  location.href = "https://scrapbox.io/RutileYugioh/" + result;
+  const page_title_tmp = document.querySelector("#header > h1 > a").innerHTML;
+  const page_title = encodeURIComponent(page_title_tmp.replace(/《/g, "").replace(/》/g, ""));
+  let result = "[** TODO : カード画像を追加する。]\n\n";
+  const card_text = document.querySelector("#body > pre").innerHTML;
+  card_text.split("\n").forEach( function(value) {
+    result += "> " + value + "\n"
+  });
+  location.href = "https://scrapbox.io/RutileYugioh/" + page_title + "?body=" + encodeURIComponent(result);
 })();
