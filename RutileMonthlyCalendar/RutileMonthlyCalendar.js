94bcde143a1f26608571068674721da0c39dacbd
Rutile3 <Rutile3@outlook.jp>
2019-06-10 09:14:48 +0900

    月予定表へ遷移するWebページを作成しました。

---
+++ RutileMonthlyCalendar/RutileMonthlyCalendar.js
@@ -0,0 +1,9 @@
+(function () {
+  const project_url = "RutileDiary";
+
+  const date = new Date();
+  const yy = (""   +  date.getFullYear()  ).slice(-2);
+  const mm = ("00" + (date.getMonth() + 1)).slice(-2);
+
+  location.href = "https://scrapbox.io/" + project_url + "/" + yy + mm;
+})();
