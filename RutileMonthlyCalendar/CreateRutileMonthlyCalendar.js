40069a51534862695aa0b1809caaa5266b774b58
Rutile3 <Rutile3@outlook.jp>
2019-06-12 09:05:07 +0900

    Closure Compilerで圧縮しました。

--- RutileMonthlyCalendar/CreateRutileMonthlyCalendar.js
+++ RutileMonthlyCalendar/CreateRutileMonthlyCalendar.js
@@ -1,11 +1,11 @@
 (function () {
   const project_url = "RutileDiary";
 
+  const day_of_week = [ "日", "月", "火", "水", "木", "金", "土" ]
+  function getDayOfWeek(day) {return day_of_week[day] }
   function getYY(date) { return (""  + (date.getFullYear() )).slice(-2); }
   function getMM(date) { return ("0" + (date.getMonth() + 1)).slice(-2); }
   function getYYMM(date) { return getYY(date) + getMM(date)}
-  const day_of_week = [ "日", "月", "火", "水", "木", "金", "土" ]
-  function getDayOfWeek(day) {return day_of_week[day] }
 
   //今月
   const today = new Date();
@@ -21,12 +21,14 @@
   const prev_and_next_month = "[" + getYYMM(prev_month) +"]←→[" + getYYMM(next_month) + "]";
   let body = prev_and_next_month + "\n\n";
 
+  //カレンダー
   body += "[*** 20" + today_yy + "年" + today_mm + "月]\n"
   for (let i = 1; i <= new Date(today_yy, today_mm, 0).getDate(); i++) {
       const tmp_date = new Date(today_yy, today_mm, i);
       body += "[" + today_yymm + ("0" + i).slice(-2) + "] ";
       body += getDayOfWeek(tmp_date.getDay()) + " \n";
   }
+
   body += "\n" + prev_and_next_month + "\n\n";
   body += "#月予定表"
 
