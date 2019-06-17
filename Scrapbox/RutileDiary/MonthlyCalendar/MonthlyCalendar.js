b46b0ae92d9bac2a36303f4f72b882491e2e3a0e
Rutile3 <Rutile3@outlook.jp>
2019-06-17 11:22:34 +0900

    「;」抜けを修正しました。 月と日の変換の際の"00"を"0"に変更しました。

--- Scrapbox/RutileDiary/MonthlyCalendar/MonthlyCalendar.js
+++ Scrapbox/RutileDiary/MonthlyCalendar/MonthlyCalendar.js
@@ -2,8 +2,8 @@
   const project_url = "RutileDiary";
 
   const date = new Date();
-  const yy = (""   +  date.getFullYear()  ).slice(-2);
-  const mm = ("00" + (date.getMonth() + 1)).slice(-2);
+  const yy = (""  +  date.getFullYear()  ).slice(-2);
+  const mm = ("0" + (date.getMonth() + 1)).slice(-2);
 
   location.href = "https://scrapbox.io/" + project_url + "/" + yy + mm;
 })();
