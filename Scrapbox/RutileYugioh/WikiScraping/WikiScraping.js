fb047571704399fbe8484f034fcb87643153007e
Rutile3 <Rutile3@outlook.jp>
2019-06-20 13:10:52 +0900

    リンクモンスターに対応しました。

--- Scrapbox/RutileYugioh/WikiScraping/WikiScraping.js
+++ Scrapbox/RutileYugioh/WikiScraping/WikiScraping.js
@@ -1,11 +1,19 @@
 javascript: (function () {
+  // 「《」と「》」を取り除いたページタイトルを取得
   const page_title_tmp = document.querySelector("#header > h1 > a").innerHTML;
   const page_title = encodeURIComponent(page_title_tmp.replace(/《/g, "").replace(/》/g, ""));
+
   let result = "[** TODO : カード画像を追加する。]\n\n";
-  const card_text = document.querySelector("#body > pre").innerHTML;
-  card_text.split("\n").forEach( function(value) {
-    result += "> " + value + "\n"
-  });
-  result += "> [" + location.href + " 遊戯王Wikiより転載" + "]\n"
+
+  // カードのステータスや効果テキストを取得
+  for (elem of document.querySelector("#body > pre").childNodes) {
+    if (elem.nodeName === "#text") {
+      elem.nodeValue.split("\n").forEach( function(value) {
+        result += "> " + value + "\n"; // 改行されるたびに引用記法を付与
+      });
+    }
+  }
+
+  result += "> [" + location.href + " 遊戯王Wikiより転載" + "]\n";
   location.href = "https://scrapbox.io/RutileYugioh/" + page_title + "?body=" + encodeURIComponent(result);
 })();
